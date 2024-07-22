7d04c855cee82b838baa5eb6b8399481ef577e55
Jing Han, Huang <7jesstc1999@gmail.com>
2024-07-23 01:29:19 +0800

    fix: pass the selected model to parent component

--- src/components/Selection.vue
+++ src/components/Selection.vue
@@ -1,7 +1,7 @@
     
 <template>
   <div class="card flex justify-center">
-    <Select v-model="selectedOption" :options="options" optionLabel="name" placeholder="Select the translate model" class="w-full md:w-56" />
+    <Select v-model="selectedOption" :options="options" optionLabel="name" placeholder="Select the translate model" class="w-full md:w-56" @update:model-value="handleUpdate" />
   </div>
 </template>
 
@@ -11,20 +11,21 @@ import { ref, defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'Selection',
-  emits: ['updatedModel'],
+  emits: ['updated'],
   setup(_, { emit }) {
     interface ModelOpt {
       name: String;
       code: String;
     }
     const options = ref<ModelOpt[]>([
-      { name: 'New York', code: 'NY' },
-      { name: 'Rome', code: 'RM' },
+      { name: 'llama2-zh-to-en (Chinese to English)', code: 'English' },
+      { name: 'llama2-en-to-zh (English to Chinese)', code: 'Chinese' },
     ]);
     const selectedOption = ref<string>();
 
-    function handleUpdate(val: string):void {
-      emit('updatedModel', val);
+    function handleUpdate(val: ModelOpt):void {
+      console.log(val.code)
+      emit('updated', val.code);
     }
 
     return {
