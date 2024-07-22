cb816e046b6952d0f02eafa1bab51531a0e7d6ef
Jing Han, Huang <7jesstc1999@gmail.com>
2024-07-23 04:01:09 +0800

    fix: optimize the model props

--- src/components/Selection.vue
+++ src/components/Selection.vue
@@ -19,7 +19,7 @@ export default defineComponent({
     }
     const options = ref<ModelOpt[]>([
       { name: 'llama2-zh-to-en (Chinese to English)', code: 'English' },
-      { name: 'llama2-en-to-zh (English to Chinese)', code: '繁體中文' },
+      { name: 'llama2-en-to-zh (English to Chinese)', code: 'Chinese' },
     ]);
     const selectedOption = ref<string>();
 
--- src/services/ollamaService.ts
+++ src/services/ollamaService.ts
@@ -1,11 +1,18 @@
 import ollama from 'ollama'
 
+const Models: Record<string, string>  = {
+  "Chinese": "請幫我將以下英文文本翻譯成繁體中文，並且回傳的文字僅有翻譯後的繁體中文。英文文本為：",
+  "English": "Please translate the following content from Chinese to English. You should only response translated English. The Chinese content is: ",
+};
+
 export async function translateService(text: string, targetLanguage: string): Promise<string> {
   try {
     console.log("--------translating-------")
+    console.log(Models[targetLanguage] + text)
+
     const response = await ollama.chat({
         model: 'llama2',
-        messages: [{ role: 'user', content: 'translate the following content to ' + targetLanguage + '(You should only response the translated content):' + text }],
+        messages: [{ role: 'user', content: Models[targetLanguage] + text }],
       })
     return response.message.content.trim() || '';
   } catch (error) {
