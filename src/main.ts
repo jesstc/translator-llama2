82d4b2b1ec8eb4446a738cbcbce9ad6ec14483f8
Jing Han, Huang <7jesstc1999@gmail.com>
2024-07-23 00:56:42 +0800

    chore: add primevue in main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,54 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+// primevue 
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
+import Menubar from 'primevue/menubar';
+import SelectButton from 'primevue/selectbutton';
+import Stepper from 'primevue/stepper';
+import StepItem from 'primevue/stepitem';
+import Step from 'primevue/step';
+import StepPanel from 'primevue/steppanel';
+import Button from 'primevue/button';
+import Select from 'primevue/select';
+import Textarea from 'primevue/textarea';
+import Message from 'primevue/message';
+import FileUpload from 'primevue/fileupload';
+import Toast from 'primevue/toast';
+import ToastService from 'primevue/toastservice';
+// PrimeVue CSS
+import 'primeicons/primeicons.css';                          // Icons
+import 'primeflex/primeflex.css';                            // PrimeFlex
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura,
+        options: {
+            prefix: 'p',
+            darkModeSelector: '.p-dark',
+            cssLayer: false
+        }
+    }
+});
+app.use(ToastService);
+
+app.component('Menubar', Menubar);
+app.component('SelectButton', SelectButton);
+app.component('Stepper', Stepper);
+app.component('StepItem', StepItem);
+app.component('Step', Step);
+app.component('StepPanel', StepPanel);
+app.component('Button', Button);
+app.component('Select', Select);
+app.component('Textarea', Textarea);
+app.component('Message', Message);
+app.component('FileUpload', FileUpload);
+app.component('Toast', Toast);
 
 app.mount('#app')
