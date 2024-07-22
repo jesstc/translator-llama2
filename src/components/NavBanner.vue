ca0b687095cc119f49c4e59cf7cb876f98c59efd
Jing Han, Huang <7jesstc1999@gmail.com>
2024-07-23 01:05:40 +0800

    feat: add NavBanner component

---
+++ src/components/NavBanner.vue
@@ -0,0 +1,17 @@
+  <template>
+    <Menubar class="surface-overlay p-3 shadow-2 flex align-items-center justify-content-between relative lg:static">
+      <template #start>
+        <img height="30" viewBox="0 0 35 40" fill="none" src="@/assets/logo.png" alt="Logo" class="logo" />
+      </template>
+    </Menubar>
+  </template>
+  
+  <script lang="ts">
+  import { defineComponent } from 'vue';
+
+  export default defineComponent({
+    name: 'NavBanner',
+  });
+  </script>
+  
+  
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import ChatBox from '../components/ChatBox.vue'
+import NavBanner from '../components/NavBanner.vue';
 </script>
 
 <template>
   <main>
+    <NavBanner />
     <ChatBox />
   </main>
 </template>
