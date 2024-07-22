946fe19ad287f3465be8ac30b58a6dedc1e9e7c3
Jing Han, Huang <7jesstc1999@gmail.com>
2024-07-23 01:11:15 +0800

    feat: add TranslateSteps component

---
+++ src/components/TranslateSteps.vue
@@ -0,0 +1,87 @@
+    
+<template>
+
+<div class="mt-3">
+  <Stepper value="1">
+    <StepItem value="1">
+      <Step><b class="text-lg">Select The Translate Type: </b></Step>
+      <StepPanel>
+        <ButtonList @updated="onUpdateSelect" />
+      </StepPanel>
+    </StepItem>
+  </Stepper>
+</div>
+
+<div class="mt-3">
+  <Stepper value="2" >
+    <StepItem value="2">
+      <Step><b class="text-lg">Select The Translate Model: </b></Step>
+      <StepPanel>
+        <Selection @updated="onUpdateModel" />
+      </StepPanel>
+    </StepItem>
+  </Stepper>
+</div>
+
+<div class="mt-3">
+  <Stepper value="3">
+    <StepItem value="3">
+      <Step><b class="text-lg">Enter / Upload the Content You Want to Translate: </b></Step>
+      <StepPanel>
+        <div class="grid grid-nogutter mt-2 align-items-start" v-if="translateText">
+          <TextareaBox class="col" label="Original Content" />
+          <div class="col-1 text-center align-self-center">
+            <i class="pi pi-arrow-right" style="color: 'var(--p-primary-color)'"></i>
+          </div>
+          <TextareaBox class="col" label="Translated Content" :isTranslated="true" />
+        </div>
+        <div class="grid grid-nogutter mt-2 align-items-start" v-else>
+          <TextareaBox class="col" label="Original Content" />
+          <div class="col-1 text-center align-self-center">
+            <i class="pi pi-arrow-right" style="color: 'var(--p-primary-color)'">aaa</i>
+          </div>
+          <TextareaBox class="col" label="Translated Content" :isTranslated="true" />
+        </div>
+      </StepPanel>
+    </StepItem>
+  </Stepper>
+</div>
+
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import ButtonList from './ButtonList.vue';
+import Selection from './Selection.vue';
+import TextareaBox from './TextareaBox.vue';
+
+export default defineComponent({
+  name: 'TranslateSteps',
+  components: {
+    ButtonList,
+    Selection,
+    TextareaBox,
+  },
+  setup() {
+    const translateText = ref<boolean>(true);
+
+    function onUpdateSelect(selectedVal: string):void {
+      console.log("father(type): ", selectedVal);
+      if (selectedVal != null) {
+        selectedVal == 'Text' && (translateText.value = true);
+        selectedVal == 'Excel File' && (translateText.value = false);
+      }
+    }
+
+    function onUpdateModel(selectedVal: string):void {
+      console.log("father(model): ", selectedVal);
+    }
+
+    return {
+      translateText,  // true: translate text, false: translate file
+      onUpdateSelect,
+      onUpdateModel,
+    };
+  }
+});
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,16 @@
 <script setup lang="ts">
 import ChatBox from '../components/ChatBox.vue'
 import NavBanner from '../components/NavBanner.vue';
+import TranslateSteps from '../components/TranslateSteps.vue';
 </script>
 
 <template>
   <main>
     <NavBanner />
+    <div class="flex-col p-4 text-left">
+      <h2 class="m-2">Translation Assistant</h2>
+      <TranslateSteps />
+    </div>
     <ChatBox />
   </main>
 </template>
